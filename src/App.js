c35adc7b2323ef96292fab7928b41543008b9eed
Jack Mittelmeier <jackmittelmeier@email.arizona.edu>
2019-12-29 14:31:47 -0700

    add about section

--- src/App.js
+++ src/App.js
@@ -3,19 +3,45 @@ import logo from './logo.svg';
 import './App.css';
 import exStyles from './style.js';
 import { withStyles } from "@material-ui/core/styles";
+import Container from "./Container"
 class MainPage extends Component {
   constructor(props) {
     super(props);
   }
+
+
+
   render() {
     const { classes } = this.props;
     return(
       <div className={classes.main}>
-        <h3> 
-            Jack Mittelmeier 
-            University of Arizona
+        <Container className={classes.header}>
+              Jack Mittelmeier
+        </Container>
+        <Container className={classes.about}>
+        <p 
+            style={{float: 'left', marginLeft: 25,}}
+          >
+          I am currently a senior in computer science at the University of Arizona. I work at CyVerse Data Science where I create react 
+          javascript components, API endpoints in Go Lang, and GUI / API regression tests for the Discovery Enironment.
+          I have many interests beyond programming such as bouldering and reading that you can check out on the left.
+        </p>
+          <img 
+            style = {{
+              marginLeft: 15,
+              width: '40vw',
+              height: '30vh',
+              float: 'right',
+              marginRight: 100,
+              marginBottom: 25,
+            }}
+          src="/images/portrait.jpg"/>
+        </Container>
+        <Container>
+          
+          </Container>
 
-        </h3>
+        
 
       </div>
     );
--- src/style.js
+++ src/style.js
@@ -1,7 +1,21 @@
 export default (theme) => ({
     main: {
-        position: "absolute",
         backgroundColor: "#E0EDF5",
         textAlign: "center",
+        float: 'none',
+        margin: '0 auto',
+        fontFamily: "Roboto",
+        marginTop: 15,
+    },
+
+    header: {
+        fontSize: 50,
+        fontFamily: "Roboto",
+    },
+
+    about: {
+        fontFamily: "Roboto",
+        marginTop: 40,
+        fontSize: 25,
     }
 });
