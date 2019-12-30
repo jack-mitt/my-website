08f2e2e04b8325ff86fee1160b8bcad437fddc34
Jack Mittelmeier <jackmittelmeier@email.arizona.edu>
2019-12-29 21:47:22 -0800

    add github button

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import exStyles from './style.js';
 import { withStyles } from "@material-ui/core/styles";
-import Container from "./Container"
+import Container from "./Container";
+import GitHubButton from 'react-github-btn';
 class MainPage extends Component {
   constructor(props) {
     super(props);
@@ -20,8 +21,8 @@ class MainPage extends Component {
         </Container>
         <Container className={classes.about}>
         <p>
-          I am currently a senior in computer science at the University of Arizona. I work at CyVerse Data Science where I create react 
-          javascript components, API endpoints in Go Lang, and GUI / API regression tests for the Discovery Enironment.
+          I am currently a senior in computer science at the University of Arizona. I work at <a href="https://cyverse.org/">CyVerse Data Science</a> where I create react 
+          javascript components, API endpoints in Go Lang, and GUI / API regression tests for the <a href="https://de.cyverse.org/de/" >Discovery Enironment</a>.
           I have many interests beyond programming such as bouldering and reading that you can check out on the left.
         </p>
           
@@ -37,6 +38,16 @@ class MainPage extends Component {
             }}
           src="/images/portrait.jpg"/>
           </Container>
+
+        <Container>
+          <GitHubButton 
+            href="https://github.com/jack-mitt" 
+            data-color-scheme="no-preference: dark; light: dark; dark: dark;" 
+            data-size="large" 
+            aria-label="Follow @jack-mitt on GitHub">
+            Follow @jack-mitt
+          </GitHubButton>
+        </Container>
 
         
 
